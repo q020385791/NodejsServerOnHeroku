6948fbf4cffc70912f504d275946f55aa427d385
unknown <q020385791@gmail.com>
2022-03-01 21:40:50 +0800

    merge update

--- Server.js
+++ Server.js
@@ -1,14 +1,27 @@
 const http=require("http");
-// const { request } = require("https");
-
+var MongoClient = require('mongodb').MongoClient;
+var cors = require('cors');
+//雲端 Atlas
+var DBurl = "mongodb+srv://sa:sa@cluster0.0qrl0.mongodb.net/CustomSystem?retryWrites=true&w=majority";
+const DbName = "CustomSystem";
+const CollectionName = "User";
 const server=http.createServer((request,response)=>{
+
+
 // response.end("hello nodejs server");
-    
     if (request.method == "POST") {
         if (request.url=="/AddUser")
         {
-            response.writeHead(200, { 'Content-Type': 'application/json' });
-            response.end(JSON.stringify({ message: "Hello World POST" }));
+            const chunks = [];
+            request.on('data', chunk => {
+            console.log('A chunk of data has arrived: ', chunk.toString());
+            var data = JSON.parse(chunk.toString());
+            console.log(data.firstName);
+            InsertUser(data.firstName)
+            response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Credentials": "true", 'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS' });
+            response.end(JSON.stringify({ message: 'Hello World POST' + data.firstName }));
+          
+            });
             // response.end();
 
         }
@@ -16,12 +29,10 @@ const server=http.createServer((request,response)=>{
 
     if (request.method == "GET") 
     {
-        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Credentials": "true", 'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS'});
         response.end(JSON.stringify({ message: "Hello World" }));
         //response.end('Get received');
-
     }
-
 }); 
     
 
@@ -34,3 +45,16 @@ server.listen(PORT,()=>
     console.log(`Server is running at port:'${PORT}`); 
 }); 
 
+//輸入一筆資料
+function InsertUser(name) {
+    MongoClient.connect(DBurl, function (err, db) {
+        if (err) throw err;
+        var newobj = { name: name, password: "123", Tel: "912345678" };
+        var dbo = db.db(DbName);
+        dbo.collection(CollectionName).insertOne(newobj, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+}
